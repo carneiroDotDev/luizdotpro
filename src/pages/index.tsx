55779fa84169eef2b579c19a8fd98f98d4c8caa4
Luiz Carneiro <luuizpaulo@gmail.com>
2019-10-24 20:31:45 +0200

    All converted to typescript

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    <SEO title="Home" description="" lang="" meta={[{ name: "", content: ""}]}></SEO>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>

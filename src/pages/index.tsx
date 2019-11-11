4f954cfcba6c759e8471f8bb7b7bb68d19c437de
Luiz Carneiro <luuizpaulo@gmail.com>
2019-11-11 23:50:42 +0100

    Added react-helmet

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,12 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Head from "../components/Head"
 
 const IndexPage = (): ReactElement => (
   <Layout>
     <SEO title="Home" description="" lang="" meta={[{ name: "", content: ""}]}></SEO>
+    <Head />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
